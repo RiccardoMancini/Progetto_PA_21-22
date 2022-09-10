2b11b3d4cbb2c79e08917efeffc93b8d25aad7bc
Riccardo Mancini <riccardomancini98@gmail.com>
2022-09-10 09:49:09 +0200

    Create postgres db with first seed

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
-const express = require('express');
-import * as userController from './controller/userController';
-import { isAdmin } from './middleware/auth';
-import { validateData } from './middleware/validator';
+import express from "express";
  
 // Constants
 const PORT = 8080;
@@ -10,34 +7,10 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(express.json());
 
-/*app.get('/area', isAdminCheck, (req, res) => {  
-  res.status(200)
-  .end('Questa area personale è di: ' + req.decoded.username + '\n' + 'email: ' + req.decoded.email);
-});
-
-app.post('/addOrder', [isAdminCheck,  orderController.newOrder]);
-
-app.get('/orders', orderController.orderList);
-
-*/
-
 
 app.get('/', (req: any, res: any) => {
   res.send('Benvenuto alla home di Express!');
 });
-
-app.get('/users', userController.userList);
-
-app.post('/addCredit', [isAdmin, validateData, userController.addCredit]);
-
-app.get('/users/:id', userController.getUserById);
-
-
-
-app.use((err,req,res, next) => {
-  res.status(err.status).send(err.stack);
-})
-
 
 
 app.listen(PORT, HOST);
---
+++ scripts/full.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE pa;
+\c pa
+
+CREATE TABLE Utenti(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    cognome VARCHAR(30) NOT NULL,
+    ruolo int NOT NULL,
+    credito int NOT NULL
+);
+
+CREATE TABLE Chiavi (
+    chiavi_id SERIAL PRIMARY KEY,
+    public_key VARCHAR(50) NOT NULL,
+    private_key VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Asta (
+    asta_id SERIAL PRIMARY KEY,
+    tipo int NOT NULL,
+    p_min int NOT NULL,
+    stato int NOT NULL,
+    data_i date NOT NULL,
+    data_f date NOT NULL,
+    chiavi_id int,
+    CONSTRAINT FK_Chiavi FOREIGN KEY (chiavi_id)
+    REFERENCES Chiavi(chiavi_id)
+);
+
+
+CREATE TABLE Partecipazione(
+    part_id SERIAL PRIMARY KEY,
+    user_id int NOT NULL,
+    asta_id int NOT NULL,
+    aggiudicata boolean NOT NULL,
+    offerta int NOT NULL,
+    CONSTRAINT FK_Utenti FOREIGN KEY (user_id)
+    REFERENCES Utenti(user_id),
+    CONSTRAINT FK_Asta FOREIGN KEY (asta_id)
+    REFERENCES Asta(asta_id)
+);
+
+INSERT INTO Utenti (username, nome, cognome, ruolo, credito) VALUES
+('richymanci', 'Riccardo', 'Mancini', 2, 50), 
+('marti_pa', 'Martina', 'Paoletti', 3, 0),
+('arme_peliv', 'Arment', 'Pelivani', 2, 70),
+('admin', 'AD', 'MIN', 1, 999),
+('pippo', 'Pippo', 'Pallino', 3, 25);
+
+INSERT INTO Asta (tipo, p_min, stato, data_i, data_f) VALUES
+(2, 15, 3, '2020-12-15', '2020-12-17'),
+(3, 10, 3, '2021-11-13', '2021-12-13'),
+(2, 50, 1, '2022-06-27', '2022-12-17');
+
+INSERT INTO Partecipazione (user_id, asta_id, aggiudicata, offerta) VALUES
+(2, 2, FALSE, 40),
+(5, 1, TRUE, 20),
+(1, 2, FALSE, 15);
