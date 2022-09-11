b2fc192f528d1f01dcde7bda9cf4065b70e9ef90
Riccardo Mancini <riccardomancini98@gmail.com>
2022-09-11 11:35:34 +0200

    testing crypto library

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { DB_Connection } from "./src/config/db_connection";
 import { Controller } from "./src/controllers/controller";
 import { checkHeader, checkToken, isBidCreator } from "./src/middleware/middlewareAuth";
+import crypto from 'crypto'
+
 const controller = new Controller();
 
 // Constants
@@ -11,7 +13,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(express.json());
 
-app.use([checkHeader, checkToken]);
+//app.use([checkHeader, checkToken]);
 
 
 app.get('/', (req: any, res: any) => {
@@ -28,6 +30,34 @@ app.get('/part', isBidCreator, (req, res) => {
 });
 
 
+const privateKey = '-----BEGIN PRIVATE KEY-----\n'+'MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAMMsV0E0hp8aCCW8QdTLUfl7lEzUzLbSyO/Y0XehBa8x+BWJav9gMlbp1GiL2oXeM2CKCRGVVJBnHSoBl+3x/l4q9BASVtMkLbWoe62OaHPZakN7o4rfil7KyIAER+rOqDtnJmtXhc2ZeA/1olUQ4FAQterj0CbAgVE2DroWMZXVAgMBAAECgYEAqhkR7p01mqIYabLZ+Pg+eF1w0UlBJTEtuINRg55NkvPM0BBUtT8EgGUIHxmMGNGgG0oSP0XTTorfiwWagOFAGw8xEDxRbiJNisN7LW9J9M+HV3ZU4SIv05Bp4LgLcqOPVhdFz/DGn5CRFC4WSFNtlM0T+8cTDY2QecQxf6Ow1p0CQQD94TVA3wCoKpEvNMKGJcJtvptKkE38Rw2evsxhff+Rj+drCFycsW7RUeLbZqQinuhDZ25fus1eIFaAnfdXwDkTAkEAxM2ec9ugQESu0N/BcQIcbmrWg9IhO7vgM5DTRl5bKxYbRoYkiWY0d1XUipUEH3bOzO5aHbpjWFbFs4eqJUx6dwJBAK0BbzzJurTebRlyJq0rxhbrMP/a3onwEYQAGV7GcgZLvjPd+t58uzvBRYqlo2xERZrJPRnXPmPKPahf/VG7GCkCQGQe61arzAoGJx2SR/ozBb3S0pE7jLaz7Oqu6h0pkZ4I4BVv1xSjj1uKqdda2j8blTfCpsvq7/GYK3obMZ9KGh8CQQDzrvgOblEt7y2f7R1GGX0lKC03X4g3aouOhtbuN1b3pp8jPqwQbWCNYb7BwG91S9vZGK/yQ2e5gNvS93BG6ATH\n'+'-----END PRIVATE KEY-----';
+const text_b64 = 'VCVcDQTEKUUIr27fOiz1KIslA2fiUqF36ZR+PYhI9Imy9H1d8QtLLx7rcETTDnYWITwtAeAUlvYQPrEtx9MDmbJrGlqcsljycmWYFgGJAHA/w+oYZJu9A/YR1vgM3sAIFGbOO1/eQDLWax62jsVC6Se/BPAHP00DYWsY5Z+Jum0='
+
+
+/*const encryptedData = crypto.publicEncrypt(
+  {
+    key: publicKey,
+    padding: crypto.constants.RSA_PKCS1_PADDING
+  },
+  // We convert the data string to a buffer using `Buffer.from`
+  Buffer.from(data)
+);
+
+
+console.log("encypted data: ", Buffer.from(encryptedData.toString("base64"), 'base64'));
+console.log("encypted data: ", encryptedData);*/
+
+const decryptedData = crypto.privateDecrypt(
+  {
+    key: privateKey,
+    padding: crypto.constants.RSA_PKCS1_PADDING
+  },
+  Buffer.from(text_b64, 'base64')
+);
+
+// The decrypted data is of the Buffer type, which we can convert to a
+// string to reveal the original data
+console.log("decrypted data: ", decryptedData.toString());
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
