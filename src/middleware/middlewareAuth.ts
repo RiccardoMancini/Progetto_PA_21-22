3a6375ff37694baef312f07783c5baf22df79825
Riccardo Mancini <riccardomancini98@gmail.com>
2022-09-12 12:54:48 +0200

    added another middleware and develope a route for filered list of aste

--- src/middleware/middlewareAuth.ts
+++ src/middleware/middlewareAuth.ts
@@ -34,14 +34,34 @@ export const checkToken = (req,res,next) => {
         else res.status(401).send('Undefined');
 };
 
+export const verifyAndAuthenticate = (req,res,next) => {
+    try{
+        let decoded = jwt.verify(req.token, process.env.SECRET_KEY);
+        if(decoded !== null){
+            if((decoded.role === "admin" || 
+                decoded.role === "bid_partecipant" || 
+                decoded.role === "bid_creator") &&
+                typeof decoded.username === "string"){
+                 req.user = decoded.username
+                 next()
+            } else {
+                //DA GESTIRE CON L'ERROR HANDLER
+                res.status(500).send('No permessi')
+            }
+        }else{
+            res.status(500).send('Errore di firma')
+        }
+    }catch(error){
+        console.log('Sono nel catch!')
+    }
+};
+
 /**
  * Funzione che verifica la correttezza del token JWT e decodifica il payload.
  * @param req 
  * @param res 
  * @param next 
  */
-
-
 export const isBidCreator = (req,res,next) => {
     let decoded = jwt.verify(req.token, process.env.SECRET_KEY);    
     if(decoded !== null && typeof decoded.username === "string" && (decoded.role === 1 || decoded.role === 2)) {
