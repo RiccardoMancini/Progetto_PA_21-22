e8b14c74e5a03fb8c776bde1ec6952b95de7a33b
armentpelivani <s1081838@studenti.univpm.it>
2022-09-14 23:13:02 +0200

    Update proxyAsta.ts

--- src/proxy/proxyAsta.ts
+++ src/proxy/proxyAsta.ts
@@ -1,32 +1,12 @@
 import { DB_Connection } from '../config/db_connection'
 import { Asta } from "../models/asta";
 
-const dateCheck = (date:String) => {
-    var sData = date.split(' ',3);
-    var nData:String = "";
-
-    
-        if ((sData[1].length===4)
-            && (sData[3].length >=1 && sData[2].length <= 2 && Number(sData[2])<=12 && Number(sData[2])>=1)
-            && (sData[3].length >=1 && sData[3].length <= 2 && Number(sData[3])<=31 && Number(sData[2])>=1)){
-            var cData:String[]=[sData[3],sData[2],sData[1]];// riordinami la data
-            var nData:String=cData.join(' ');
-            return nData;
-        }
-        else{
-            throw new TypeError('The credito is not an integer');
-        }
-    
-
-}
 const __Handler = {
     get: (obj, prop) => { 
         if(prop === 'tipo' ){
             if(!Number.isInteger(obj[prop])){
                 throw new TypeError("formato dell'asta non valido");
             }
-
-            return obj[prop];
         }
 
         if(prop ==='data_i' ){        
@@ -41,15 +21,11 @@ const __Handler = {
                 throw new TypeError('la data non è corretta')
 
             }
-
-            return dateCheck(obj[prop]);
         }
         if(prop ==='data_f' ){
             if (!Number.isInteger(obj[prop])) {
                 throw new TypeError('la data non è corretta');
             }
-
-            return obj[prop];
         }    
 
         if(prop ==='p_min'){
@@ -57,10 +33,7 @@ const __Handler = {
                 throw new TypeError('il prezzo di base non è valido');
             }
 
-            return obj[prop];
-
         }
-
     }              
 }
 
@@ -75,22 +48,7 @@ export class ProxyAsta{
 
     }
 
-    public async getOpenAstaByID(asta_id: number){
-        const asta =  await this.modelAsta.getOpenAstaByID(asta_id);
-        return this.checkOpenAsta(asta) === true? asta : console.log('ERRORE: ASTA NON ESISTENTE'); 
-    }
-
     public async getAste(){
         return this.modelAsta.getAste();
     }
-
-    public async createAsta(asta:any){
-        // CHECK DELL'OGGETTO ASTA CON PROXY()
-        return await this.modelAsta.createAsta(asta);
-    }
-
-    public checkOpenAsta(asta: any){
-        return asta !== null? true : false;
-      }
-
 }
