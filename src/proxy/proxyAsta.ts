c1f93e4efa577a739391b40384757c2d6b1714b2
armentpelivani <s1081838@studenti.univpm.it>
2022-09-14 23:13:02 +0200

    Update proxyAsta.ts

--- src/proxy/proxyAsta.ts
+++ src/proxy/proxyAsta.ts
@@ -2,12 +2,39 @@ import { DB_Connection } from '../config/db_connection'
 import { Asta } from "../models/asta";
 
 const __Handler = {
-    get: (obj, prop) => {                
-        
-        /**
-         * DA FARE
-         */
-    }
+    get: (obj, prop) => { 
+        if(prop === 'tipo' ){
+            if(!Number.isInteger(obj[prop])){
+                throw new TypeError("formato dell'asta non valido");
+            }
+        }
+
+        if(prop ==='data_i' ){        
+            if (!Number.isInteger(obj[prop])&& (Date.now()>obj[prop])
+                                            && (obj['data_i']) >= obj['data_f']
+                                            && (obj['tipo']===1)) {
+                throw new TypeError('la data non è corretta');
+            }
+            else if(!Number.isInteger(obj[prop])&&(obj['tipo']!==1) 
+                                                && (Date.now()>obj[prop])
+                                                && (obj['data_i']) >= obj['data_f']){
+                throw new TypeError('la data non è corretta')
+
+            }
+        }
+        if(prop ==='data_f' ){
+            if (!Number.isInteger(obj[prop])) {
+                throw new TypeError('la data non è corretta');
+            }
+        }    
+
+        if(prop ==='p_min'){
+            if ((!Number.isInteger(obj[prop])) && obj[prop]<=0) {
+                throw new TypeError('il prezzo di base non è valido');
+            }
+
+        }
+    }              
 }
 
 
